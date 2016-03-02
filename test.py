2edca024df33d123b89f23df4151f5bfcafb8ed5
Massih M <massih.z@gmail.com>
2016-03-02 15:15:17 +0100

    receive from socket

--- test.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-if 'SUMO_HOME' in os.environ:
-  print "true"
-     #tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-     #sys.path.append(tools)
-else:
-  print "false"   
-     #sys.exit("please declare environment variable 'SUMO_HOME'")
