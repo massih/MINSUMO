5d10eecb54f41cce2b5b99d3d7fe1c9b61224075
Massih M <massih.z@gmail.com>
2016-05-07 19:25:07 +0200

    RSU finder

--- dataCategorizer/src/main/java/com/minsumo/datacategorizer/CategorizingBolt.java
+++ dataCategorizer/src/main/java/com/minsumo/datacategorizer/CategorizingBolt.java
@@ -1,28 +1,102 @@
 package com.minsumo.datacategorizer;
 
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
+import com.github.davidmoten.grumpy.core.Position;
+import com.github.davidmoten.rtree.Entry;
+import com.github.davidmoten.rtree.RTree;
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Point;
+import com.github.davidmoten.rtree.geometry.Rectangle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rx.Observable;
+import rx.functions.Func1;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by massih on 4/26/16.
  */
-public class CategorizingBolt extends BaseBasicBolt {
+
+public class CategorizingBolt extends BaseRichBolt {
+
+    private OutputCollector collector;
+    private final double COVERAGE_AREA = 0.25;
+    private RTree<String, Point> tree;
+
+    private static final Logger LOG = LoggerFactory.getLogger(CategorizingBolt.class);
+
+    public CategorizingBolt() {
+        //this.tree = tree;
+    }
 
     @Override
-    public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
-        /*
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        collector = outputCollector;
+        RTree<String, Point> tree = (RTree<String, Point>)map.get("treeObject");
+        System.out.println("************ size of tree ====== " +tree.size()+" ***********");
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+               /*
         "timeStep"
         "vehicleID"
         "speed"
         "longitude"
         "latitude"
         */
+        Double lon = Double.parseDouble(tuple.getString(3));
+        Double lat = Double.parseDouble(tuple.getString(4));
+        Point vPoint = Point.create(lon, lat);
+        List<Entry<String, Point>> rsuList = search(tree, vPoint, COVERAGE_AREA).toList().toBlocking().single();
+        System.out.println("*******Found RSU num: " + rsuList.size() + " *******");
     }
+
+
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
 
+    }
+
+    private <T> Observable<Entry<T, Point>> search(RTree<T, Point> tree, Point lonLat,
+                                                   final double distanceKm) {
+        // First we need to calculate an enclosing lat long rectangle for this
+        // distance then we refine on the exact distance
+        final Position from = Position.create(lonLat.y(), lonLat.x());
+        Rectangle bounds = createBounds(from, distanceKm);
+
+        return tree
+                // do the first search using the bounds
+                .search(bounds)
+                // refine using the exact distance
+                .filter(new Func1<Entry<T, Point>, Boolean>() {
+                    @Override
+                    public Boolean call(Entry<T, Point> entry) {
+                        Point p = entry.geometry();
+                        Position position = Position.create(p.y(), p.x());
+                        return from.getDistanceToKm(position) < distanceKm;
+                    }
+                });
+    }
+
+    private Rectangle createBounds(final Position from, final double distanceKm) {
+        // this calculates a pretty accurate bounding box. Depending on the
+        // performance you require you wouldn't have to be this accurate because
+        // accuracy is enforced later
+        Position north = from.predict(distanceKm, 0);
+        Position south = from.predict(distanceKm, 180);
+        Position east = from.predict(distanceKm, 90);
+        Position west = from.predict(distanceKm, 270);
+
+        return Geometries.rectangle(west.getLon(), south.getLat(), east.getLon(), north.getLat());
     }
 }
--- dataCategorizer/src/main/java/com/minsumo/datacategorizer/DCtopology.java
+++ dataCategorizer/src/main/java/com/minsumo/datacategorizer/DCtopology.java
@@ -4,11 +4,18 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.topology.TopologyBuilder;
+import com.github.davidmoten.rtree.RTree;
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Point;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import storm.kafka.*;
 import storm.kafka.trident.GlobalPartitionInformation;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -38,6 +45,9 @@ public class DCtopology {
         SpoutConfig spoutConfig = new SpoutConfig(staticHosts, KAFKA_TOPIC_NAME, ZK_ROOT, UUID.randomUUID().toString());
         spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
         spoutConfig.startOffsetTime = kafka.api.OffsetRequest.LatestTime();
+        //Reading RSU file
+        RTree<String, Point> tree = makeTree("RSUs.txt");
+        conf.put("treeObject", tree);
         //TOPOLOGY COMPONENTS
         builder.setSpout(KAFKA_SPOUT, new KafkaSpout(spoutConfig), 1);
         builder.setBolt(FILTERING_BOLT, new FilteringBolt(), 1).shuffleGrouping(KAFKA_SPOUT);
@@ -45,5 +55,27 @@ public class DCtopology {
 
         LocalCluster cluster = new LocalCluster();
         cluster.submitTopology(TOPOLOGY_NAME, conf, builder.createTopology());
+    }
+
+    private static RTree<String, Point> makeTree(String path){
+        RTree<String, Point> tree = RTree.star().create();
+        try (BufferedReader bufferReader = new BufferedReader(new FileReader(path)) ){
+            String[] idLatLon;
+            String id;
+            double lat,lon;
+            String line = bufferReader.readLine();
+            while (line != null) {
+                idLatLon = line.split(",");
+                //System.out.println(idLatLon[0] + " " + idLatLon[1] + " " + idLatLon[2]);
+                id = idLatLon[0];
+                lat = Double.parseDouble(idLatLon[1]);
+                lon = Double.parseDouble(idLatLon[2]);
+                tree = tree.add(id, Geometries.pointGeographic(lon, lat));
+                line = bufferReader.readLine();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return tree;
     }
 }
